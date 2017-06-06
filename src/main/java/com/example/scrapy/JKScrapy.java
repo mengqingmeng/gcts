55c42bd3410917d21308d0d027a72e2bdfc7bdd7
mengqingmeng <762577099@qq.com>
2017-06-06 14:14:43 +0800

    pom

--- src/main/java/com/example/scrapy/JKScrapy.java
+++ src/main/java/com/example/scrapy/JKScrapy.java
@@ -56,9 +56,9 @@ public class JKScrapy {
         } catch (Exception e) {
             boolean failure = true;
             //e.printStackTrace();
-            logger.info("第"+pageIndex+"页，请求失败，将在5min后再次请求。");
+            logger.info("第"+pageIndex+"页，请求失败，将在1min后再次请求。");
             try {
-                Thread.sleep(300000);
+                Thread.sleep(60000);
             } catch (InterruptedException ie) {
                 ie.printStackTrace();
             }
@@ -92,9 +92,9 @@ public class JKScrapy {
                 product = getProduct(baseUrl+urlAndParams[0],urlAndParams[1]);
             } catch (Exception e) {
                 //e.printStackTrace();
-                logger.info("获取第"+pageIndex+"页的产品失败一次，将在5min后重新请求。详情："+urlAndParams[0]+urlAndParams[1]);
+                logger.info("获取第"+pageIndex+"页的产品失败一次，将在1min后重新请求。详情："+urlAndParams[0]+urlAndParams[1]);
                 try {
-                    Thread.sleep(300000);
+                    Thread.sleep(60000);
                 } catch (InterruptedException ie) {
                     ie.printStackTrace();
                 }
